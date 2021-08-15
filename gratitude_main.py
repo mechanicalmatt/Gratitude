de2e236a1a18421b75232eaab70b24d4200da6aa
mechanicalmatt <matt.ta.gray@gmail.com>
2021-08-15 16:22:23 -0700

    added return random entry fxn

--- gratitude_main.py
+++ gratitude_main.py
@@ -1,13 +1,28 @@
-
-
-import time
-
+from datetime import date
+import random
 
 db = "C:/Users/m_gray.DESKTOP-Q2TTLA7/Desktop/gratitude.txt"
 day = ""
+today = date.today()
+
+random_entry = ""
+all_entries = []
 
 
-info = input('what are you grateful for today? -> ')
+# print entry from random date
+def rand_selection():
+    with open(db, 'r') as f:
+        count = 0
+        for entry in f.readlines():
+            count += 1
+            all_entries.append(entry)
+        selection = random.randrange(count)
+    all_entries_strip = [i.strip('\n') for i in all_entries]
+    return all_entries_strip[selection]
+print(rand_selection())
+print()
+
+info = [str(today) + ': ' + input('what are you grateful for today? -> ')]
 
 def append(text):
     with open(db, 'a') as f:
@@ -28,4 +43,3 @@ def run():
 # read()
 run()
 # read()
-
