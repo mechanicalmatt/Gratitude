542a468c0de17845319b60922f620473ab328c15
mechanicalmatt <matt.ta.gray@gmail.com>
2021-08-15 15:23:44 -0700

    basic fxn

---
+++ gratitude_main.py
@@ -0,0 +1,28 @@
+import time
+
+
+db = "C:/Users/m_gray.DESKTOP-Q2TTLA7/Desktop/gratitude.txt"
+day = ""
+
+
+info = input('what are you grateful for today? -> ')
+
+def append(input):
+    with open(db, 'a') as f:
+        print(input('what are you grateful for today? -> '), file=f)
+
+def read():
+    with open(db, 'r') as f:
+        for i in f.readlines():
+            print(i)
+        input('...')
+
+def run():
+    if info == 'READ':
+        print(read())
+    else:
+        append(info)
+
+# read()
+run()
+# read()
